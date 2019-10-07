84a958e7a805a5fc13c8b69b02f60a55ccc44859
Matthew H. McKenzie <33498537+swellhunter@users.noreply.github.com>
2019-10-08 07:12:33 +1100

    Not rebooting at all.

--- i2cScan.ino
+++ i2cScan.ino
@@ -1,7 +1,7 @@
 /****************************************************
   https://github.com/swellhunter/i2cScan
   i2cScan - Ripped off from Arduino Playground
-  Adapted for ATTiny85 with UART LCD or terminal.
+  Adapted for ATTiny85 with RayLid UART LCD.
   ___    +---v---+
   RST   1|o      |8  VCC
   XTAL  2|       |7  SCL  * two wire serial clock
@@ -11,40 +11,70 @@
   Requires :
   https://github.com/nickgammon/SendOnlySoftwareSerial
   https://github.com/SpenceKonde/ATTinyCore
-  add the following URL to boards manager:
-  http://drazzy.com/package_drazzy.com_index.json
+  Raylid RSI1602***-00 16*02 UART LCD display.
+  (from Ebay user surenoo)
+  2 x 4.7KΩ pull up resistors for IIC lines.
+  Physical switch for power on and off. 
 ******************************************************/
 #include <Wire.h>
-#include <avr/wdt.h>
 #include <SendOnlySoftwareSerial.h>
 
+/* Globals - well "ours" anyway */
 SendOnlySoftwareSerial mySerial (1);  // Tx pin
 
-// This is the standard "run once" routine.
 void setup() {
-  // https://bigdanzblog.wordpress.com/2015/07/20/resetting-rebooting-attiny85-with-watchdog-timer-wdt/
-  wdt_disable();
   mySerial.begin(9600);
-  brightness(63);
+  brightness_lcd(63);
   Wire.begin();
   delay(1000);
+  preamble();
+}
+
+void loop() {
+
+  byte error, address;
+  int nDevices;
+  char all[17] = "";
+  char buff[3] = "";
 
   clear_lcd();
   beginLCDWrite(0, 0);
-  mySerial.print(F("RAMEND : 0x0"));
-  mySerial.print(RAMEND, HEX);
-  endLCDWrite();
-  beginLCDWrite(1, 0);
-  mySerial.print(F("Stack  : 0x0"));
-  mySerial.print(SP, HEX);
+  mySerial.print(F("Scanning...   "));
   endLCDWrite();
 
-  delay(8000);
+  nDevices = 0;
+/****************************************************
+  https://github.com/swellhunter/i2cScan
+  i2cScan - Ripped off from Arduino Playground
+  Adapted for ATTiny85 with RayLid UART LCD.
+  ___    +---v---+
+  RST   1|o      |8  VCC
+  XTAL  2|       |7  SCL  * two wire serial clock
+  XTAL  3|       |6  TX   * UART serial out
+  GND   4|       |5  SDA  * two wire serial data
+         +-------+
+  Requires :
+  https://github.com/nickgammon/SendOnlySoftwareSerial
+  https://github.com/SpenceKonde/ATTinyCore
+  Raylid RSI1602***-00 16*02 UART LCD display.
+  (from Ebay user surenoo)
+  2 x 4.7KΩ pull up resistors for IIC lines.
+  Physical switch for power on and off. 
+******************************************************/
+#include <Wire.h>
+#include <SendOnlySoftwareSerial.h>
+
+/* Globals - well "ours" anyway */
+SendOnlySoftwareSerial mySerial (1);  // Tx pin
+
+void setup() {
+  mySerial.begin(9600);
+  brightness_lcd(63);
+  Wire.begin();
+  delay(1000);
   preamble();
-
 }
 
-// This is the endless loop body
 void loop() {
 
   byte error, address;
@@ -59,7 +89,7 @@ void loop() {
 
   nDevices = 0;
 
-  for (address = 1; address < 128; address++) {
+  for (address = 0; address < 128; address++) {
 
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
@@ -73,7 +103,11 @@ void loop() {
 
       beginLCDWrite(1, 0);
       mySerial.print(F("Found 0x"));
-      if (address < 16) mySerial.print(F("0"));
+
+      if (address < 16) {
+        mySerial.print(F("0"));
+      }
+
       mySerial.print(address, HEX);
       endLCDWrite();
       nDevices++;
@@ -108,16 +142,18 @@ void loop() {
     mySerial.print(F("Done. "));
     mySerial.print(nDevices);
     mySerial.print(F(" hit"));
-    if (nDevices > 1) mySerial.print(F("s"));
+
+    if (nDevices > 1) {
+      mySerial.print(F("s"));
+    }
+
     mySerial.print(F("."));
   }
 
   endLCDWrite();
   delay(8000);
-  reminder();
-  delay(4000);
-  reboot2();
-
+  clear_lcd();
+  delay(200);
 }
 
 void clear_lcd(void) {
@@ -160,9 +196,6 @@ void endLCDWrite(void) {
    Things in setup() best abstracted
 */
 void preamble(void) {
-  mySerial.write(0xAA);               // Hardware info.
-  mySerial.write((uint8_t)0x00);
-  delay(2000);
   clear_lcd();
   beginLCDWrite(0, 0);
   mySerial.print(F("*** I2C Scan ***"));
@@ -174,48 +207,131 @@ void preamble(void) {
   delay(1000);
 }
 
-void brightness(unsigned short x) {
+void brightness_lcd(unsigned short x) {
   mySerial.write(0xAA);
   mySerial.write(0x13);
   mySerial.write(x);
 }
+  for (address = 0; address < 128; address++) {
 
-//void reBootTiny85() {
-// Quick, dirty, effective....
-// This is why people prefer WDT ?
-//  SREG  = 0;
-//  MCUSR = 0;
-//  SP    = RAMEND;
-//  asm("rjmp 0");
-//}
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
 
-void reminder(void) {
+    if (error == 0) { // knock was answered
+
+      if (nDevices) strcat(all, ",");
+      strcat(all, "0x");
+      sprintf(buff, "%02X", address);
+      strcat(all, buff);
+
+      beginLCDWrite(1, 0);
+      mySerial.print(F("Found 0x"));
+
+      if (address < 16) {
+        mySerial.print(F("0"));
+      }
+
+      mySerial.print(address, HEX);
+      endLCDWrite();
+      nDevices++;
+      delay(2000);
+      clear_lcd_line2();
+    }
+    else if (error == 4) {
+      beginLCDWrite(1, 0);
+      mySerial.print(F("Error at : 0x"));
+      if (address < 16) mySerial.print(F("0"));
+      mySerial.print(address, HEX);
+      endLCDWrite();
+      delay(4000);
+      clear_lcd_line2();
+    }
+  }
+
+  if (nDevices == 0) {
+    clear_lcd();
+    beginLCDWrite(0, 0);
+    mySerial.print(F("No I2C devices!!"));
+  }
+  else {
+    delay(1000);
+    clear_lcd();
+
+    beginLCDWrite(0, 0);
+    mySerial.print(all);
+    endLCDWrite();
+
+    beginLCDWrite(1, 0);
+    mySerial.print(F("Done. "));
+    mySerial.print(nDevices);
+    mySerial.print(F(" hit"));
+
+    if (nDevices > 1) {
+      mySerial.print(F("s"));
+    }
+
+    mySerial.print(F("."));
+  }
+
+  endLCDWrite();
+  delay(8000);
   clear_lcd();
-  delay(250);
-  beginLCDWrite(0, 0);
-  mySerial.print(F("** Please -   **"));
-  endLCDWrite();
-  beginLCDWrite(1, 0);
-  mySerial.print(F("** switch off **"));
-  endLCDWrite();
+  delay(200);
 }
 
-void reboot2() {
-#if defined (__AVR_ATtiny85__)
-  cli();
-  // WDIF = TRUE    refer "Watchdog Timer Control Register"
-  // WDIE = TRUE
-  // WDP3   not set
-  // WDCE = TRUE
-  // WDE  = TRUE
-  // WDP2,WDP1,WDP0 = 110
-  WDTCR = 0xD8 | WDTO_1S;
-  sei();
-  wdt_reset();
-#else
-  wdt_disable();
-  wdt_enable(WDTO_15MS);
-#endif
-  // trap the sparks and wait.
-  while (true) {}
+void clear_lcd(void) {
+  mySerial.write(0xAA);
+  mySerial.write(0x10);
+}
+
+void clear_lcd_line2(void) {
+
+  unsigned short i;
+
+  beginLCDWrite(1, 0);
+  for (i = 0; i < 16; i++) {
+    mySerial.print(F(" "));
+  }
+  endLCDWrite();
+  delay(500);
+}
+
+void beginLCDWrite(unsigned char r, unsigned c) {
+  mySerial.write(0xAA);
+  mySerial.write(0x20);
+  mySerial.write(r);
+  mySerial.write(c);
+  mySerial.write(0xAA);
+  mySerial.write(0x25);
+}
+
+/*
+   So the million dollar question is can
+   this just be replaced with \n?
+   Best to keep it in case of differing
+   hardware.
+*/
+void endLCDWrite(void) {
+  mySerial.write(0x0D);
+}
+
+/*
+   Things in setup() best abstracted
+*/
+void preamble(void) {
+  clear_lcd();
+  beginLCDWrite(0, 0);
+  mySerial.print(F("*** I2C Scan ***"));
+  endLCDWrite();
+  delay(500);
+  beginLCDWrite(1, 0);
+  mySerial.print(F("... stand by ..."));
+  endLCDWrite();
+  delay(1000);
+}
+
+void brightness_lcd(unsigned short x) {
+  mySerial.write(0xAA);
+  mySerial.write(0x13);
+  mySerial.write(x);
 }
